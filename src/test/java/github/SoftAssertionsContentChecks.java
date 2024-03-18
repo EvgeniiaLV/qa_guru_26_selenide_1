de57b7094266338b2ca18ba3272d94e33c18be62
Evgeniia Liasheva <evgenia_lozovik@mail.ru>
2024-03-18 10:00:22 +0300

    JUnit5 example code added

--- src/test/java/github/SoftAssertionsContentChecks.java
+++ src/test/java/github/SoftAssertionsContentChecks.java
@@ -1,7 +1,8 @@
 package github;
 
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.exist;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -12,7 +13,7 @@ public class SoftAssertionsContentChecks {
     void JUnit5ExampleExistsInSoftAssertions(){
         // open the Selenide page
         open("https://github.com/selenide/selenide");
-        // open Wiki tab
+        // click on Wiki tab
         $("a#wiki-tab").click();
 
         // search for soft assertions page
@@ -20,7 +21,19 @@ public class SoftAssertionsContentChecks {
         // click on the Soft Assertions page
         $(byAttribute("data-filterable-for", "wiki-pages-filter")).$(byTagAndText("a", "SoftAssertions")).click();
 
-        // check that JUnit5 example exists
+        // check that the JUnit5 example exists
         $(withTagAndText("h4","Using JUnit5 extend test class")).should(exist);
-    }
+        $("#wiki-body").shouldHave(text("""
+               @ExtendWith({SoftAssertsExtension.class})
+                class Tests {
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }"""));
+   }
 }
